8799e76617efac9d9ed7eb61aa10d55a2382bd7e
Konik Rahman <konikrahman06@gmail.com>
2024-05-12 01:19:17 +0300

    12/05/24, addition.js: create a function for calculate the two number and pass it

---
+++ JavaScript-Practicing-Exercise/addition.js
@@ -0,0 +1,13 @@
+
+
+function myAddition(){
+
+    let valueOne, valueTwo, valueThree;
+
+    valueOne = 10;
+    valueTwo = 12;
+    valueThree = valueOne + valueTwo;
+
+    document.getElementById('addition').innerHTML = valueThree;
+
+}
